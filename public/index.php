6359de48a0d37ed4ea4ead8f45e3eada8168edcb
ro <rl@smartpointlab.com>
2021-11-18 21:38:56 +0200

    day1 end

--- public/index.php
+++ public/index.php
@@ -1,9 +1,10 @@
 <?php
 declare(strict_types = 1);
 
-use Laminas\Diactoros\Request;
 use Laminas\Diactoros\Response\HtmlResponse;
 use Laminas\Diactoros\ServerRequestFactory;
+use Laminas\HttpHandlerRunner\Emitter\SapiEmitter;
+
 ini_set('display_errors', 'on');
 //
 chdir(dirname(__DIR__));
@@ -13,3 +14,7 @@ $request = ServerRequestFactory::fromGlobals();
 
 $name = $request->getQueryParams()['name']??'Guest';
 
+$response = (new HtmlResponse('Hello, '. $name .'!'))->withHeader('X-Developer','RoLu');
+
+$emitter = new SapiEmitter();
+$emitter->emit($response);
